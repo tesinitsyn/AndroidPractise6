1fa224dbe611459feb962696ba76b7782e70ba9e
tesinitsyn <sitiev174@gmail.com>
2023-04-14 22:53:03 +0300

    Added showNotification method, added onClickListener for notificationButton

--- app/src/main/java/com/example/androidpractise5/ChooseRole.java
+++ app/src/main/java/com/example/androidpractise5/ChooseRole.java
@@ -1,7 +1,13 @@
 package com.example.androidpractise5;
 
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -11,8 +17,33 @@ import android.view.ViewGroup;
 
 
 public class ChooseRole extends Fragment {
+
+    private static final int NOTIFY_ID = 0;
+    private final String CHANNEL_ID = "channel_id";
     View view;
 
+    @SuppressLint("MissingPermission")
+    public void showNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext(), CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Your first notification realisation")
+                .setContentText("It's still your first notification realisation")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getContext());
+        notificationManagerCompat.notify(NOTIFY_ID, builder.build());
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Channel name",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("Channel description");
+            NotificationManager notificationManager = getActivity().getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 
     public ChooseRole() {
         super(R.layout.fragment_choose_role);
@@ -32,6 +63,9 @@ public class ChooseRole extends Fragment {
             Bundle bundle = new Bundle();
             bundle.putString("buttonPressing", "1");
             Navigation.findNavController(view).navigate(R.id.action_chooseRole_to_registration, bundle);
+        });
+        view.findViewById(R.id.notificationButton).setOnClickListener(v ->{
+            showNotification();
         });
         return view;
     }
